46b2c6c746b4771d0e0dc762133f5646f8394a23
Mayuri Sharma <sharmamayuri98@gmail.com>
2023-09-02 21:12:36 -0400

    vps | Update for mobile

---
+++ js/index.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var mobileMenuLinks = document.getElementById('mobile-menu-links');
+  if (mobileMenuLinks.style.display === 'block') {
+    mobileMenuLinks.style.display = 'none';
+  } else {
+    mobileMenuLinks.style.display = 'block';
+  }
+}
